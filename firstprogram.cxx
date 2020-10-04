222833d39052c64d296bb549d2b249e5206a65f4
ardiantrik <ardiantrik@gmail.com>
2020-10-04 21:59:10 +0700

    cobo testing 2

--- firstprogram.cxx
+++ firstprogram.cxx
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout<<"Nama : siapa saya?"<<endl;
+	cout<<"Nama : siapa ya saya?"<<endl;
 	cout<<"NIM  : 123000000"<<endl;
 	cout<<"Dan seterusnya ...";
 	return 0;
